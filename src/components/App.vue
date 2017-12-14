90cdbc1e4e3a7621a152b3297ce67129ab02c9c1
dianziyoujian@mail.com <dianziyoujian@mail.com>
2017-12-14 20:22:22 +0800

    begin work

---
+++ src/components/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="top-box">
+    <!-- <Navbar></Navbar> -->
+    <router-view></router-view>
+    <Toaster></Toaster>
+        <Footerbar></Footerbar>
+  </div>
+</template>
+
+<script>
+import store from 'store'
+//my zone
+import Footerbar from './Footer'
+
+//import Navbar from './Navbar'
+import Toaster from './Toaster'
+
+export default {
+  store,
+ components:{ Footerbar,Toaster }
+ //components:{ Navbar,Toaster }
+}
+</script>
+
+<style>
+.main-box {
+    padding-left: 0;
+    padding-bottom:50px;
+}
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+	<footer>
+	<div class="footer-container">
+	  <ul>
+	    <li>
+	      <span>©2015 / 鄂ICP备15010989号-1</span>
+	    </li>
+	    <li>
+	      <a class="github" href="https://github.com/jackhutu/jackblog-vue" target="_blank">
+	        <i class="fa fa-github"></i>
+	      </a>
+	    </li>
+	    <li>
+	      <a class="weibo" href="http://weibo.com/hutaigong" target="_blank">
+	        <i class="fa fa-weibo"></i>
+	      </a>
+	    </li>
+	  </ul>
+	</div>
+	</footer>
+</template>
+
+<script>
+	export default {
+		
+	}
+</script>
+
+<style>
+footer {
+		width:100%;
+    display: block !important;
+    position: fixed;
+		bottom:0;
+		    background-color: white;
+		border: none;
+    box-shadow: 4px 0 4px #eee;
+
+}
+</style>
